0b3f2a1e81542f476d49d74d4cebd883ca2ae412
Samu Kangas <samu.kangas@metropolia.fi>
2024-09-22 19:32:42 +0300

    korjaus tehtävä kolmosee

--- assignment-3/src/t3.js
+++ assignment-3/src/t3.js
@@ -1,29 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 document
-  .getElementById("bookForm")
-  .addEventListener("submit", function (event) {
+    .getElementById("bookForm")
+    .addEventListener("submit", function (event) {
     event.preventDefault();
-    var titleInput = document.getElementById("title").value;
-    var authorInput = document.getElementById("author").value;
-    var yearInput = document.getElementById("year").value;
+    var titleInput = document.getElementById("title")
+        .value;
+    var authorInput = document.getElementById("author")
+        .value;
+    var yearInput = document.getElementById("year")
+        .value;
     var publicationYear = parseInt(yearInput);
     if (!titleInput || !authorInput || isNaN(publicationYear)) {
-      alert("Please fill in all fields with valid data.");
-      return;
+        alert("Please fill in all fields with valid data.");
+        return;
     }
     var book = {
-      title: titleInput,
-      author: authorInput,
-      publicationYear: publicationYear,
+        title: titleInput,
+        author: authorInput,
+        publicationYear: publicationYear,
     };
     var resultDiv = document.getElementById("result");
-    resultDiv.innerHTML =
-      "\n        <h2>Book Details:</h2>\n        <p><strong>Title:</strong> "
-        .concat(book.title, "</p>\n        <p><strong>Author:</strong> ")
-        .concat(
-          book.author,
-          "</p>\n        <p><strong>Publication Year:</strong> "
-        )
-        .concat(book.publicationYear, "</p>\n    ");
-  });
+    resultDiv.innerHTML = "\n        <h2>Book Details:</h2>\n        <p><strong>Title:</strong> ".concat(book.title, "</p>\n        <p><strong>Author:</strong> ").concat(book.author, "</p>\n        <p><strong>Publication Year:</strong> ").concat(book.publicationYear, "</p>\n    ");
+});
--- assignment-3/src/t3.ts
+++ assignment-3/src/t3.ts
@@ -1,5 +1,3 @@
-export {};
-
 type Book = {
   title: string;
   author: string;
